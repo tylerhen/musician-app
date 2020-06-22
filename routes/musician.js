81791d0e8df5c07222332e295f3d368f80d7c5f2
Tyler Hennessey <tyler.hennessey@daicompanies.com>
2020-06-22 09:05:19 -0700

    I changed some shit yo

--- routes/musician.js
+++ routes/musician.js
@@ -9,6 +9,12 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
+// healthcheck
+router.get('/health', (req, res) => {
+  res.status('200').send("Status OK");
+});
+
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
